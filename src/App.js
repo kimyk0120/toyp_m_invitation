7ab82969de2179088886c0cda7642e8a346c7810
kimyk0120 <kimyk0120@hotmail.com>
2021-12-16 14:41:39 +0900

    변경

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import {Fragment} from "react";
 function App() {
 
     let test_name = 'test_name_3'
-    
+    let test_name2 = 'test_name_4'
+    const value = 1;
 
     return (
-        <Fragment>
-            <input type="text"/>
-            <div>{test_name}</div>
-            <div>test2</div>
-        </Fragment>
+        <div>
+            {/*{1 + 1 === 3 ? (<div>yes</div>) : (<div>no</div>)}*/}
+            {/* 주석처리 */}
+            {/*{1 + 1 === 2 && (<div>yes</div>)}*/}
+            <div className={"App"}>
+                {
+                    (function() {
+                        if (value === 1) return (<div>하나</div>);
+                        if (value === 2) return (<div>둘</div>);
+                        if (value === 3) return (<div>셋</div>);
+                    })()
+                }
+            </div>
+        </div>
         // <div className="App">
         //     <header className="App-header">
         //         <img src={logo} className="App-logo" alt="logo"/>
---
+++ src/MyName.js
@@ -0,0 +1,7 @@
+import React, {Component} from "react";
+
+class MyName extends Component {
+
+}
+
+export default MyName;
