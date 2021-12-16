4bdeb3d8b8118de32eb04eab386e359ef4451da2
kimyk0120 <kimyk0120@hotmail.com>
2021-12-16 17:17:04 +0900

    study

--- src/MyName.js
+++ src/MyName.js
@@ -1,7 +1,25 @@
-import React, {Component} from "react";
+// import React, {Component} from "react";
 
-class MyName extends Component {
+// class MyName extends Component {
+//     static defaultProps = {name:'defaultNm'}
+//
+//     render() {
+//         return (
+//             <div>
+//                 name :  <b>{this.props.name}</b>
+//             </div>
+//         )
+//     }
+// }
 
+const MyName = ({name}) => {
+
+    return (
+        <div>
+            name_prop : {name}
+        </div>
+    )
 }
+
 
 export default MyName;
