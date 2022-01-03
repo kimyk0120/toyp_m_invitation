6dc45a83a5ebd07869128a13dff3acad787edfa2
kimyk0120 <kimyk0120@hotmail.com>
2022-01-03 17:14:10 +0900

    - react study

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
+// noinspection JSCheckFunctionSignatures
+
 import logo from './logo.svg';
 import './App.css';
 import {Component, Fragment} from "react";
 // import MyName from "./MyName";
 // import Counter from "./Counter";
 import PhoneForm from "./components/PhoneForm";
+import PhoneInfoList from "./components/PhoneInfoList";
 
 class App extends Component {
+    id = 2
+
+    state = {
+        information: [
+            {
+                id: 0,
+                name: '김민준',
+                phone: '010-0000-0000'
+            },
+            {
+                id: 1,
+                name: '홍길동',
+                phone: '010-0000-0001'
+            }
+        ]
+    }
 
     handleCreate = (data) => {
-        console.log(data);
+        // console.log(data);
+        const {information} = this.state;
+        this.setState({
+            information: information.concat({id: this.id++, ...data})
+        })
     }
     render() {
+        const {information} = this.state;
         return (
             <div>
                 <PhoneForm phonform_onCreate={this.handleCreate} />
+                <PhoneInfoList data={this.state.information} />
+                {/*{JSON.stringify(information)}*/}
             </div>
+
             // <Fragment>
             //     <MyName name={"react_2"} />
             //     <Counter />
